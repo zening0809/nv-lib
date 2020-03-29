a041b7162cc1b544b2e87b4537bf3b8bb62b67d3
泽宁 <752671125@qq.com>
2020-03-29 13:34:14 +0800

    feat: 增加commit 限制

--- src/internal/getTag.ts
+++ src/internal/getTag.ts
@@ -9,6 +9,7 @@ const toString = Object.prototype.toString
  */
 function getTag(value: any): string {
     if (value == null) {
+        
         return value === undefined ? '[object Undefined]' : '[object Null]'
     }
     return toString.call(value)
