bde461f8ca874a21b184eea24c528587f7168bd4
泽宁 <752671125@qq.com>
2020-03-29 14:12:30 +0800

    feat: eslint 修复

--- src/internal/getTag.ts
+++ src/internal/getTag.ts
@@ -7,9 +7,8 @@ const toString = Object.prototype.toString
  * @param {*} value The value to query.
  * @returns {string} Returns the `toStringTag`.
  */
-function getTag(value:           any): string {
+function getTag(value: any): string {
     if (value == null) {
-
         return value === undefined ? '[object Undefined]' : '[object Null]'
     }
     return toString.call(value)
