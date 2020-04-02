6d99f4e86b91345dcbf3ceaab383fd36a57c6842
泽宁 <752671125@qq.com>
2020-04-02 12:32:52 +0800

    fix: 增加 nodetype v10支持

--- src/internal/nodeTypes.ts
+++ src/internal/nodeTypes.ts
@@ -1,8 +1,20 @@
+import freeGlobal from './freeGlobal'
+
+interface Options {
+    [key: string]: any;
+}
+
 /** Detect free variable `exports`. */
 const freeExports = typeof exports === 'object' && exports !== null && !exports.nodeType && exports
 
 /** Detect free variable `module`. */
 const freeModule = freeExports && typeof module === 'object' && module !== null && module
+
+/** Detect the popular CommonJS extension `module.exports`. */
+const moduleExports = freeModule && freeModule.exports === freeExports
+
+/** Detect free variable `process` from Node.js. */
+const freeProcess: Options = moduleExports && freeGlobal.process
 
 /** Used to access faster Node.js helpers. */
 const nodeTypes = (((): any => {
@@ -11,6 +23,9 @@ const nodeTypes = (((): any => {
     /* Node.js deprecation code: DEP0103. */
     const typesHelper = freeModule && freeModule.require && freeModule.require('util').types
     return typesHelper
+      ? typesHelper
+      /* Legacy process.binding('util') for Node.js earlier than v10. */
+      : freeProcess && freeProcess.binding && freeProcess.binding('util')
   } catch (e) {}
 })())
 
